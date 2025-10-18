3c59cc0e080d8bdba48a544456f731d6a626e0a5
mila-z <m_zaprianova@protonmail.com>
2025-10-18 20:48:06 +0300

    optimize

--- hw_1/frog_leap_puzzle.py
+++ hw_1/frog_leap_puzzle.py
@@ -1,38 +1,30 @@
 def is_goal_state(board, zero_state):
-    return zero_state == len(board)//2 and not('>' in board[:zero_state]) and not('<' in board[(zero_state + 1):])
+    mid = len(board)//2
+    return (
+        zero_state == mid and 
+        '>' not in board[:mid] and 
+        '<' not in board[mid + 1:])
 
 def moves(board, zero_state):
-    moves = []
-    board = list(board)
-    board_len = len(board)
+    res = []
+    n = len(board)
+
+    def swap(new_pos):
+        temp = list(board)
+        temp[zero_state], temp[new_pos] = temp[new_pos], temp[zero_state]
+        return tuple(temp)
 
     # think of better var names
-    right = zero_state - 1
-    double_right = zero_state - 2
-    left  = zero_state + 1
-    double_left = zero_state + 2
+    for step in (1, 2):
+        jump = zero_state - step
+        if jump >= 0 and board[jump] == '>':
+            res.append((swap(jump), jump))
 
-    if right >= 0 and board[right] == '>':
-        board_right = board.copy()
-        board_right[right], board_right[zero_state] = board_right[zero_state], board_right[right]
-        moves.append((tuple(board_right), right))
-    
-    if double_right >= 0 and board[double_right] == '>':
-        board_double_right = board.copy()
-        board_double_right[double_right], board_double_right[zero_state] = board_double_right[zero_state], board_double_right[double_right]
-        moves.append((tuple(board_double_right), double_right))
+        jump = zero_state + step
+        if jump < n and board[jump] == '<':
+            res.append((swap(jump), jump))
 
-    if left < board_len and board[left] == '<':
-        board_left = board.copy()
-        board_left[left], board_left[zero_state] = board_left[zero_state], board_left[left]
-        moves.append((tuple(board_left), left))
-
-    if double_left < board_len and board[double_left] == '<':
-        board_double_left = board.copy()
-        board_double_left[double_left], board_double_left[zero_state] = board_double_left[zero_state], board_double_left[double_left]
-        moves.append((tuple(board_double_left), double_left))
-
-    return moves
+    return res
     
 
 def dfs(board, zero_state, visited=None, path=None):
@@ -44,23 +36,24 @@ def dfs(board, zero_state, visited=None, path=None):
     if board in visited:
         return None
     visited.add(board)
+    path.append(''.join(board))
 
     if is_goal_state(board, zero_state):
-        return path + [''.join(board)]
+        return path.copy() #why a copy
     
     for move, new_state in moves(board, zero_state):
-        result = dfs(move, new_state, visited, path + [''.join(board)])
+        result = dfs(move, new_state, visited, path)
         if result:
             return result
         
-
+    path.pop()
     return None
 
 def get_board(n):
     return tuple(['>']*n + ['_'] + ['<']*n)
 
 n = int(input())
-zero_state = n
+# zero_state = n
 board = get_board(n)
 # board_tuple = tuple(board)
 # print(board_tuple)
@@ -68,7 +61,7 @@ board = get_board(n)
 # print(moves(board, zero_state))
 # print(is_goal_state(('<', '<', '_', '>', '>'), 2))
 
-sol = dfs(board, zero_state)
+sol = dfs(board, n)
 if sol:
     for step in sol:
         print(step)
